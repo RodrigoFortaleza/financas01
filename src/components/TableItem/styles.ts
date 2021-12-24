3479534818952460cb17dc185d7000c00b8669ca
Rodrigo Fortaleza <rodrigo-edufortaleza@hotmail.com>
2021-12-24 09:44:16 -0300

    primeiro commit

---
+++ src/components/TableItem/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const TableLine = styled.tr``;
+
+export const TableColumn = styled.td`
+    padding: 10px;
+    font-weight: bold;
+`;
+
+export const Category = styled.div< { color: string }>`
+    display: inline-block;
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: #FFF;
+    background-color: ${props => props.color};
+    font-weight: bold;
+`;
+
+export const Value = styled.div < { color: string }> `
+    color: ${props => props.color};
+    font-weight: bold;
+`;
---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import { Item } from '../types/Item';
+
+
+export const items: Item[] = [
+  { date: new Date(2021, 9, 6), category: 'food', title: 'Subway', value: 32.50 },
+  { date: new Date(2021, 9, 15), category: 'food', title: 'Tasty Grill', value: 18 },
+  { date: new Date(2021, 9, 15), category: 'rent', title: 'Aluguel Apt', value: 350 },
+  { date: new Date(2021, 10, 19), category: 'salary', title: 'Salário ACME', value: 1300 },
+];
